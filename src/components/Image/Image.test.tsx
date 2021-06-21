748f80f78ac6c1b4cd2cc5795fd2482406b7d167
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-06-21 05:34:39 +0530

    Setup boilerplate

--- src/components/Image/Image.test.tsx
+++ src/components/Image/Image.test.tsx
@@ -3,38 +3,6 @@ import { render } from '@testing-library/react';
 
 import { Image } from './';
 
-const imgSrc = 'https://memegenerator.net/img/instances/50262258.jpg';
-const altText = 'this is literally a meme';
-const fallbackSrc =
-  'https://www.androidpolice.com/wp-content/uploads/2018/11/chrome-dino-hero.png';
+import { fallbackSrc, photoSrc, loadingSrc } from '../../constants';
 
-describe('Image', () => {
-  it('should render the image properly', async () => {
-    const { getByAltText } = render(<Image src={imgSrc} alt={altText} />);
-
-    const image = getByAltText(altText);
-
-    expect(image).toBeTruthy();
-  });
-
-  it('should apply the provided height', async () => {
-    const height = '200px';
-    const { getByAltText } = render(
-      <Image src={imgSrc} alt={altText} height={height} />
-    );
-
-    const image = getByAltText(altText);
-
-    expect(image.style.height).toEqual(height);
-  });
-
-  it('should handle errors and render the fallback', async () => {
-    const { getByAltText } = render(<Image src="#" alt={altText} />);
-
-    const image = getByAltText(altText) as HTMLImageElement;
-
-    setTimeout(() => {
-      expect(image.src).toEqual(fallbackSrc);
-    }, 1000);
-  });
-});
+describe('Image', () => {});
--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
 import { fallbackSrc, loadingSrc } from '../../constants';
 
-export interface ImageProps {
-  src: string;
-  alt: string;
-  height?: string | number;
-}
+export interface ImageProps {}
 
-export const Image: React.FC<ImageProps> = ({ src, alt, height = '400px' }) => {
-  const [err, setErr] = React.useState(false);
-  const [loading, setLoading] = React.useState(true);
-
-  return (
-    <>
-      <img
-        style={{ height, width: 'auto', borderRadius: '10px' }}
-        onLoad={() => setLoading(false)}
-        onError={() => setErr(true)}
-        src={!err ? (loading ? loadingSrc : src) : fallbackSrc}
-        alt={alt}
-      />
-      <span>{loading}</span>
-    </>
-  );
+export const Image: React.FC<ImageProps> = ({}) => {
+  return <></>;
 };
